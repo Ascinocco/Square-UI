a4619cbd030cd33299914e198c37673569ee3337
Anthony Scinocco <scinocco.a@gmail.com>
2017-12-13 19:54:31 -0500

    added font awesome support

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
+import faStyles from 'font-awesome/css/font-awesome.css';
+
 import logo from './logo.svg';
 import styles from './App.css';
 
 const App = () => (
-  <div className={styles.App}>
+  <div className={styles.app}>
     <header className={styles['app-header']}>
       <img src={logo} className={styles['app-logo']} alt={'logo'} />
       <h1 className={styles['App-title']}>Welcome to React</h1>
     </header>
     <p className={styles['app-intro']}>
       To get started, edit <code>src/App.js</code> and save to reload.
-      <i className={'fa fa-address-card'} aria-hidden={'true'} />
     </p>
+    <div>
+      Meet Anthony
+      <FontAwesome
+        cssModule={faStyles}
+        name={'linkedin'}
+      />
+    </div>
   </div>
 );
 
