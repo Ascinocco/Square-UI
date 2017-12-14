fbfb15497e1a45ceaeaab3e7a2ef81c054154c2d
Anthony Scinocco <scinocco.a@gmail.com>
2017-12-13 19:27:09 -0500

    updated eslint

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import styles from './App.css';
 const App = () => (
   <div className={styles.App}>
     <header className={styles['app-header']}>
-      <img src={logo} className={styles['app-logo']} alt='logo' />
+      <img src={logo} className={styles['app-logo']} alt={'logo'} />
       <h1 className={styles['App-title']}>Welcome to React</h1>
     </header>
     <p className={styles['app-intro']}>
       To get started, edit <code>src/App.js</code> and save to reload.
-      <i className='fa fa-address-card' aria-hidden='true' />
+      <i className={'fa fa-address-card'} aria-hidden={'true'} />
     </p>
   </div>
 );
