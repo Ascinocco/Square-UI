482e177853473248ce3cb74f22667767ae974ffc
Anthony Scinocco <scinocco.a@gmail.com>
2017-12-13 19:20:38 -0500

    large refactoring to get damn icons

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import styles from './App.css';
 
 const App = () => (
   <div className={styles.App}>
-    <header className={styles['App-header']}>
-      <img src={logo} className={styles['App-logo']} alt='logo' />
+    <header className={styles['app-header']}>
+      <img src={logo} className={styles['app-logo']} alt='logo' />
       <h1 className={styles['App-title']}>Welcome to React</h1>
     </header>
-    <p className={styles['App-intro']}>
+    <p className={styles['app-intro']}>
       To get started, edit <code>src/App.js</code> and save to reload.
+      <i className='fa fa-address-card' aria-hidden='true' />
     </p>
   </div>
 );
